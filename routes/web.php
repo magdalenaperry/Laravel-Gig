d0137bae79c459e01a20f431e397d5bf972b13b6
Magdalena Perry <magperry16@gmail.com>
2022-10-06 00:16:57 -0400

    log out functionality

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,8 @@ Route::get('/register', [UserController::class, 'create']);
 // create new user
 Route::post('/users', [UserController::class, 'store']);
 
-
+// log user out
+Route::post('/logout', [UserController::class, 'logout']);
 
 
 
